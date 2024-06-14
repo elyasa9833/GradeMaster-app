3e89175557558ba0f286e66f157dbd7d9c743b24
Amri Elyasa <aelyasa09@gmail.com>
2024-06-15 01:34:49 +0700

    feat: add action show, delete

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <ul class="list-none m-0 p-0 bg-slate-800 overflow-hidden">
     <li class="float-left hover:bg-slate-700"><a class="active block text-white text-center px-3 py-4 no-underline" href="/student">Lihat siswa</a></li>
     <li class="float-left hover:bg-slate-700"><a class="active block text-white text-center px-3 py-4 no-underline" href="/student/create">Tambah siswa</a></li>
-    <li class="float-left hover:bg-slate-700"><a class="active block text-white text-center px-3 py-4 no-underline" href="/grade/create">Tambah nilai</a></li>
+    <li class="float-left hover:bg-slate-700"><a class="active block text-white text-center px-3 py-4 no-underline" href="/score/create">Tambah nilai</a></li>
 </ul>
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-<table class="w-full border-collapse">
+<table class="max-w-screen-lg w-full mx-auto border-collapse">
     <tr>
         <th class="border p-2 text-left bg-slate-300 w-1">No</th>
         <th class="border p-2 text-left bg-slate-300">Nama</th>
         <th class="border p-2 text-left bg-slate-300">Kelas</th>
+        <th class="border p-2 text-left bg-slate-300 w-24">Action</th>
     </tr>
 
     @foreach ($students as $i => $student)
@@ -13,6 +14,10 @@
             <td class="border p-2">{{ $i+1 }}</td>
             <td class="border p-2">{{ $student['nama'] }}</td>
             <td class="border p-2">{{ $student['kelas'] }}</td>
+            <td class="border p-2">
+                <a href="#"><img src="{{ asset('svg/show-icons.svg') }}" alt="show" class="inline-block"></a>
+                <a href="#"><img src="{{ asset('svg/delete-icons.svg') }}" alt="delete" class="inline-block"></a>
+            </td>
         </tr>
         
     @endforeach
